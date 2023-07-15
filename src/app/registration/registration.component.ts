da31274f4aed42f26800ceaedb068e62fc4f7892
Houssemkais <houssem.kais@esprit.tn>
2023-07-15 18:19:18 +0200

    cc

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,48 +8,48 @@ import { FormControl, Validators,FormGroup } from '@angular/forms';
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
-  registrationStatus: string | null = null;
-  goToLoginPage() {
-    // Redirection vers la page de connexion
-    window.location.href = "";
-  }
+export class RegistrationComponent {}
+//   registrationStatus: string | null = null;
+//   goToLoginPage() {
+//     // Redirection vers la page de connexion
+//     window.location.href = "";
+//   }
 
 
 
   
-  user = {
-    id: '',
-    lastname: '',
-    firstname: '',
-    email: '',
-    dateOfBirth: '',
-    phone: '',
-    address: '',
-    password: '',
-    userRole: '',
-    sexe:''
-  };
+//   user = {
+//     id: '',
+//     lastname: '',
+//     firstname: '',
+//     email: '',
+//     dateOfBirth: '',
+//     phone: '',
+//     address: '',
+//     password: '',
+//     userRole: '',
+//     sexe:''
+//   };
 
-  constructor(private userService: UserService) {}
+//   constructor(private userService: UserService) {}
 
-  onSubmit() {
-    this.userService.registerUser(this.user)
-      .subscribe(
-        response => {
-          this.registrationStatus = 'Utilisateur enregistré avec succès';
-          console.log('Utilisateur enregistré avec succès', response);
-        },
-        error => {
-          if (error.status === 409) {
-            this.registrationStatus = 'Utilisateur déjà ajouté';
-          } else {
-            this.registrationStatus = 'Erreur lors de l\'enregistrement de l\'utilisateur';
-          }
-          console.log('Erreur lors de l\'enregistrement de l\'utilisateur', error);
-        }
-      );
-  }
+//   onSubmit() {
+//     this.userService.registerUser(this.user)
+//       .subscribe(
+//         response => {
+//           this.registrationStatus = 'Utilisateur enregistré avec succès';
+//           console.log('Utilisateur enregistré avec succès', response);
+//         },
+//         error => {
+//           if (error.status === 409) {
+//             this.registrationStatus = 'Utilisateur déjà ajouté';
+//           } else {
+//             this.registrationStatus = 'Erreur lors de l\'enregistrement de l\'utilisateur';
+//           }
+//           console.log('Erreur lors de l\'enregistrement de l\'utilisateur', error);
+//         }
+//       );
+//   }
   
 
-}
+// }
